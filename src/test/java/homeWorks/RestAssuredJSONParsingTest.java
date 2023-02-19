5bc0f0ec35d0a7550749d98bda6c51b69396e7bb
belka <belki.quest@gmail.com>
2023-02-19 11:27:04 +0700

    Edited JSONParsingTest HW 2. Second message

--- src/test/java/homeWorks/RestAssuredJSONParsingTest.java
+++ src/test/java/homeWorks/RestAssuredJSONParsingTest.java
@@ -4,6 +4,9 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class RestAssuredJSONParsingTest {
     @Test
     public void testRestAssuredJSONParsing(){
@@ -13,24 +16,12 @@ public class RestAssuredJSONParsingTest {
         System.out.println("Первая часть задания - полученный JSON распечатать:");
         response.prettyPrint();
 
-        System.out.println("Вторая часть задания - вывести текст второго сообщения");
-        Object messages = response.get("messages");
+        System.out.println("Вторая часть задания - вывести текст второго сообщения:");
+//        Object messages = response.get("messages");
 
-
-
-//        JsonPath[] messages = new JsonPath[2];
-//        String messages = response.get("messages").toString();
-//        for (int i=0; i<1; i++){
-//            int start = messages.indexOf("{");
-//            messages = messages.substring(start);
-//        }
-//        int end = messages.indexOf("\"");
-//        char[] message2 = new char[30];
-//        messages.getChars(12, end, message2, 0);
-//        JsonPath message2T = messages[1];
-
-            System.out.println(messages);
-//        }
+        ArrayList<Map<String,String>> messages = response.get("messages");
+        Map<String,String> secondMessage = messages.get(1);
+        System.out.println(secondMessage.get("message"));
 
     }
 }
